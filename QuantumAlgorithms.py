e182ffc9514146e130a033849bd74d84689f3ed2
nic <bolton21@uwindsor.ca>
2025-01-31 00:37:43 -0500

    - outline for period finding

--- QuantumAlgorithms.py
+++ QuantumAlgorithms.py
@@ -1,5 +1,18 @@
 from qiskit import ClassicalRegister, QuantumCircuit, QuantumRegister
+import math
 
+# input: coprime integers N and a (1 < a < N)
+# output: order of a (i.e., min(r > 0) such that a^r = 1 mod N
 def find_order(a, N):
+
+    # second register size
+    m = math.ceil( math.log(N, 2) )
+
+    # first register size
+    n = 2*m
+
+    circ = QuantumCircuit(n, m)
+    circ.draw()
+
     # return the order r of a
     return 1
